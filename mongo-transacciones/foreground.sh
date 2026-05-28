2f682d0946ddadbaa87d528a477d879f49143db3
Cristian Camilo Flórez Novoa <cristian.florezn@gmail.com>
2026-05-28 16:17:14 -0500

    Se eliminan comentarios

--- mongo-transacciones/foreground.sh
+++ mongo-transacciones/foreground.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Mensajes iniciales simples
 echo "====================================================="
 echo "Configurando la arquitectura del servidor MongoDB..."
 echo "====================================================="
@@ -9,12 +8,10 @@ echo "Por favor, espera unos segundos mientras se inicializa el Replica Set (rs0
 echo "Esto es un requisito estricto para habilitar transacciones ACID en MongoDB."
 echo ""
 
-# Espera silenciosa a que el script de fondo termine, todo en una sola línea
 while [ ! -f /tmp/scenario-ready ]; do sleep 1; done
 
 clear
 
-# Mensajes de éxito simples
 echo "====================================================="
 echo "Servidor Linux y MongoDB (versión 7.0) listos."
 echo "Replica Set (rs0) inicializado correctamente."
